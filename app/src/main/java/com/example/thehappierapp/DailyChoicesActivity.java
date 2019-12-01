01358a28592e1ac8c0c7c0704cc7121d264c6fd4
Daniel-Kong <danielkong03@gmail.com>
2019-11-30 17:15:11 -0800

    added button messages

--- app/src/main/java/com/example/thehappierapp/DailyChoicesActivity.java
+++ app/src/main/java/com/example/thehappierapp/DailyChoicesActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,28 +17,37 @@ public class DailyChoicesActivity extends AppCompatActivity {
         setContentView(R.layout.daily_choices_menu);
 
         Button btn = findViewById(R.id.button_choice1);
+        final Button finalBtn1 = btn;
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                TextView text = (TextView) findViewById(R.id.placeholder);
+                text.setText(finalBtn1.getText());
                 startActivity(new Intent(DailyChoicesActivity.this, Complete.class));
             }
         });
 
         btn = findViewById(R.id.button_choice2);
+        final Button finalBtn2 = btn;
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                TextView text = (TextView) findViewById(R.id.placeholder);
+                text.setText(finalBtn2.getText());
                 startActivity(new Intent(DailyChoicesActivity.this, Complete.class));
             }
         });
 
         btn = findViewById(R.id.button_choice3);
+        final Button finalBtn3 = btn;
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                TextView text = (TextView) findViewById(R.id.placeholder);
+                text.setText(finalBtn3.getText());
                 startActivity(new Intent(DailyChoicesActivity.this, Complete.class));
             }
         });
@@ -47,7 +57,7 @@ public class DailyChoicesActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(DailyChoicesActivity.this, Complete.class));
+                startActivity(new Intent(DailyChoicesActivity.this, MainActivity.class));
             }
         });
     }
--- app/src/main/java/com/example/thehappierapp/MainActivity.java
+++ app/src/main/java/com/example/thehappierapp/MainActivity.java
@@ -13,7 +13,13 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.widget.Button;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String button1;
+    private String button2;
+    private String button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,21 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                List<Task> list;
+                try {
+                    list = Task.filter("easy");
+                } catch (Exception e) {
+                    throw new RuntimeException();
+                }
+                button1 = list.get(1).task;
+                Button buttonChoice1 = (Button) findViewById(R.id.button_choice1);
+                buttonChoice1.setText(button1);
+                button2 = list.get(2).task;
+                Button buttonChoice2 = (Button) findViewById(R.id.button_choice2);
+                buttonChoice2.setText(button2);
+                button3 = list.get(3).task;
+                Button buttonChoice3 = (Button) findViewById(R.id.button_choice3);
+                buttonChoice3.setText(button3);
                 MainActivity.this.startActivity(new Intent(MainActivity.this, DailyChoicesActivity.class));
             }
         });
@@ -33,6 +54,21 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                List<Task> list;
+                try {
+                    list = Task.filter("medium");
+                } catch (Exception e) {
+                    throw new RuntimeException();
+                }
+                button1 = list.get(1).task;
+                Button buttonChoice1 = (Button) findViewById(R.id.button_choice1);
+                buttonChoice1.setText(button1);
+                button2 = list.get(2).task;
+                Button buttonChoice2 = (Button) findViewById(R.id.button_choice2);
+                buttonChoice2.setText(button2);
+                button3 = list.get(3).task;
+                Button buttonChoice3 = (Button) findViewById(R.id.button_choice3);
+                buttonChoice3.setText(button3);
                 startActivity(new Intent(MainActivity.this, DailyChoicesActivity.class));
             }
         });
@@ -42,6 +78,21 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                List<Task> list;
+                try {
+                    list = Task.filter("hard");
+                } catch (Exception e) {
+                    throw new RuntimeException();
+                }
+                button1 = list.get(1).task;
+                Button buttonChoice1 = (Button) findViewById(R.id.button_choice1);
+                buttonChoice1.setText(button1);
+                button2 = list.get(2).task;
+                Button buttonChoice2 = (Button) findViewById(R.id.button_choice2);
+                buttonChoice2.setText(button2);
+                button3 = list.get(3).task;
+                Button buttonChoice3 = (Button) findViewById(R.id.button_choice3);
+                buttonChoice3.setText(button3);
                 startActivity(new Intent(MainActivity.this, DailyChoicesActivity.class));
             }
         });
